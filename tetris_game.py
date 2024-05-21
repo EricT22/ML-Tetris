d267d13f2e5bcf7623b213580dc8f116b7013524
eric <phantomenace87@gmail.com>
2024-05-20 17:35:56 -0700

    made sure bag logic worked, so far so good

--- tetris_game.py
+++ tetris_game.py
@@ -1,12 +1,15 @@
 from bag import Bag
+from pieces import Piece
+# from pieces import *
 from board import Board
 
 class Tetris_Game:
     def __init__(self) -> None:
         self.board = Board()
-        # self.bag = Bag()
-        # self.cur_piece = self.bag.get_next_piece()
+        self.bag = Bag()
+        self.cur_piece: Piece = self.bag.get_next_piece()
+        # self.cur_piece = O()
     
     def draw(self, screen) -> None:
+        self.cur_piece.draw_on_board(self.board)
         self.board.draw(screen)
-        # self.cur_piece.draw(screen)
