fa68441cbb5481574bfa0df4271ab281140765f0
eric <phantomenace87@gmail.com>
2024-05-21 18:06:27 -0700

    changes to files not used in build

--- usefulComments.py
+++
@@ -1,7 +0,0 @@
-# square = pygame.Rect((300, 250, 50, 50))
-# pygame.draw.rect(screen, (255, 0, 0), square)
-
-# key = pygame.key.get_pressed()
-
-# if key[pygame.K_a] == True:
-#     square.move_ip(-1, 0)
